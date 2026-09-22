d05b491434352ffe72950987ee9a3f557a1c490d
amodemoli <amirali213121@gmail.com>
2026-09-22 12:59:10 +0330

    fix: fixed some bugs, and added better varidator for timeout (because changed config timeout variable on map with any type)

--- exercise/gateway/internal/helpers/ping.go
+++ exercise/gateway/internal/helpers/ping.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/amodemoli/fastic/core/fastic"
 	"github.com/amodemoli/microservices/exercise/gateway/internal/config"
 	"github.com/valyala/fasthttp"
 )
@@ -12,7 +13,12 @@ import (
 // Ping function send's request to your target url,
 // this function record's target service response delay and show this delay to you as (ms) - milisecond
 // if your target service is offline (function get's timeout error), this function show's (-1) to you this response is milisecond
-func Ping(cnf *config.Config, client *fasthttp.Client, target string) int64 {
+func Ping(app *fastic.App, cnf *config.Config, client *fasthttp.Client, target string, customTimeout ...time.Duration) int64 {
+
+	// get final timeout from finalTimeout helper,
+	// this function have default value for timeout, first get's timeout from customTimeout
+	// and if customTimeout is empty or cannot validate it try's to get new timeout from config
+	timeout := finalTimeout(app, cnf, customTimeout[0])
 
 	// get request and response from fasthttp
 	req, resp := fasthttp.AcquireRequest(), fasthttp.AcquireResponse()
@@ -39,8 +45,8 @@ func Ping(cnf *config.Config, client *fasthttp.Client, target string) int64 {
 	start := time.Now()
 
 	// send request with timeout, used client struct for this!
-	// this line is time-out of target url you can change it on config.jsonc file
-	err := client.DoTimeout(req, resp, cnf.PingingTimeout*time.Second)
+	// this line is time-out of target url you can change it on config.jsonc file)
+	err := client.DoTimeout(req, resp, timeout)
 
 	// count response delay and save it to elapsed variable
 	elapsed := time.Since(start)
@@ -54,9 +60,70 @@ func Ping(cnf *config.Config, client *fasthttp.Client, target string) int64 {
 	return elapsed.Milliseconds()
 }
 
+// validateTimeout function maded for show to you your timeout seconds is good or not!
+// minumum value for timeout is 2 seconds and maximum is 100 seconds!
+func validateTimeout(d time.Duration) bool {
+	return d >= 2*time.Second && d <= 100*time.Second
+}
+
+// finalTimeout get's app && config model and customTimeout, and return's finally timeout Duration
+// only maded for use on Ping
+func finalTimeout(app *fastic.App, cnf *config.Config, customTimeout time.Duration) time.Duration {
+	// make variable named timeout and save default value to 10 second
+	var timeout time.Duration = 10 * time.Second
+
+	if validateTimeout(customTimeout) {
+		// save it user custom time-out
+		timeout = customTimeout
+	} else {
+		// get timeout from config file and change it to time.Duration.
+		cnfTimeout, err := toDuration(cnf.General["pinging_timeout"])
+		// if error is empty, replace it
+		if err == nil {
+			if validateTimeout(cnfTimeout) {
+				// save it config file timeout
+				timeout = cnfTimeout
+			}
+		}
+	}
+
+	// return finally timeout
+	return timeout
+}
+
+// anyToTime converts any type to time.Duration type
+// if cannot convert result is 0
+func toDuration(v any) (time.Duration, error) {
+
+	switch val := v.(type) {
+	case time.Duration:
+		return val, nil
+	case float64: // JSON numbers come as float64
+		return time.Duration(val * float64(time.Second)), nil
+	case int:
+		return time.Duration(val) * time.Second, nil
+	case int64:
+		return time.Duration(val) * time.Second, nil
+	case string:
+		return time.ParseDuration(val)
+	default:
+		return 0, fmt.Errorf("cannot convert %T to time.Duration", v)
+	}
+}
+
 // ServicePinger helper, ping's all registered services with Ping helper function's help
 // and show's response as Map
-func ServicePinger(cnf *config.Config, client *fasthttp.Client) map[string]string {
+func ServicePinger(app *fastic.App, cnf *config.Config, client *fasthttp.Client, customTimeout ...time.Duration) map[string]string {
+
+	// make, variable named timeout and setting default value to -1
+	// why -1? because -1 means i dont need to use default timeout (on validate default timeout return is false)
+	var timeout time.Duration = -1
+
+	// get length of customTimeout, dont need validate here! time validates on Ping function
+	if len(customTimeout) > 0 {
+		// replace it to custom timeout
+		timeout = customTimeout[0]
+	}
 
 	// make new variable named response for save it services ping result here
 	response := make(map[string]string, len(cnf.Services))
@@ -64,7 +131,7 @@ func ServicePinger(cnf *config.Config, client *fasthttp.Client) map[string]strin
 	// get range of registered services with service url for ping
 	for service, url := range cnf.Services {
 		// save it service to new response struct and save service response delay (ping)
-		response[service+"_service"] = fmt.Sprintf("%vms", Ping(cnf, client, url))
+		response[service+"_service"] = fmt.Sprintf("%vms", Ping(app, cnf, client, url, timeout))
 	}
 
 	// return response after write all service's response delay
