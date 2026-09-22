9f27ceed52b768cb4573ccd9c453d58bf4854e89
amodemoli <amirali213121@gmail.com>
2026-09-22 12:57:01 +0330

    feat: structure of config file's now changed and new variable's will save's on maps without need to add it automatic here

--- exercise/gateway/internal/config/load.go
+++ exercise/gateway/internal/config/load.go
@@ -6,17 +6,31 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"time"
 
 	"github.com/amodemoli/fastic/core/fastic"
 )
 
+// Config model, for save it jsonc config result here,
+// used maps because map dont need to edit result's here only can edit key on json,
+// and key && value are resets here dont need edit here before edit keys on json file
 type Config struct {
+	// Registered Services url and name are saves here
+	// used string for value type because i know only string can save it as value!
+	// value is service http server url.
 	Services map[string]string `json:"services"`
 
-	PingingTimeout time.Duration `json:"pinging_timeout"`
+	// Middleware's general settings are saves here,
+	// setting's will be used only in middlewares
+	Middewares map[string]any `json:"middlewares"`
+
+	// General settings of proxy (gateway),
+	// e.g: timeout of pinging services on health path
+	General map[string]any `json:"general"`
 }
 
+// Load function maked for return error and config model,
+// on returned config model save's jsonc config values
+// [*] you need add path of config.jsonc file and example of app model to function for work
 func Load(app *fastic.App, path string) (error, *Config) {
 	// validate config file path
 	// 1) check length only
