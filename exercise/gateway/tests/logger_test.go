2e9216872684bafc6f787982fe8657706274cce7
amodemoli <amirali213121@gmail.com>
2026-09-22 22:30:15 +0330

    feat: maked log dir with gateway.log file and, test file for testing logger section (test is not dont im working on test)

---
+++ exercise/gateway/tests/logger_test.go
@@ -0,0 +1,16 @@
+package tests
+
+import (
+	"testing"
+)
+
+func TestLogger(t *testing.T) {
+
+	/*fakeCnf := &config.Config{
+		General: map[string]any{
+
+		},
+	}
+
+	logger.New(&fastic.App{}, &config.Config{}, )*/
+}
