ebb72caeb1b69e8736bc22f6e9ee37c9061202a8
amodemoli <amirali213121@gmail.com>
2026-09-23 16:52:35 +0330

    feat: added test for logger

--- exercise/gateway/tests/logger_test.go
+++ exercise/gateway/tests/logger_test.go
@@ -1,16 +1,105 @@
 package tests
 
 import (
+	"errors"
+	"fmt"
+	"os"
 	"testing"
+	"time"
+
+	"github.com/amodemoli/fastic/core/fastic"
+	"github.com/amodemoli/microservices/exercise/gateway/internal/config"
+	"github.com/amodemoli/microservices/exercise/gateway/internal/helpers/logger"
 )
 
 func TestLogger(t *testing.T) {
 
-	/*fakeCnf := &config.Config{
-		General: map[string]any{
+	path := "./loging_test.log"
 
+	fakeCnf := &config.Config{
+		General: map[string]any{
+			"loging_file":                     path,
+			"auto_flush":                      true,
+			"development_logger_flush_ticker": "2s",
+			"production_logger_flush_ticker":  "2s",
 		},
 	}
 
-	logger.New(&fastic.App{}, &config.Config{}, )*/
+	fakeApp := &fastic.App{
+		Env: &fastic.Env{
+			DevelopemtMode: true,
+		},
+	}
+
+	err := testingLogger(fakeApp, fakeCnf, path, 1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = testingLogger(fakeApp, fakeCnf, path, 2)
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+
+func testingLogger(app *fastic.App, cnf *config.Config, path string, step int) error {
+
+	if err := os.Remove(path); err != nil {
+		return fmt.Errorf("cannot remove: %v", err)
+	}
+
+	lg := logger.New(app, cnf)
+
+	switch step {
+	case 1:
+		lg.Info("testing with flush")
+		// flush now
+		lg.Flush()
+
+		err, isEmpty := isEmpty(path)
+		if err != nil {
+			return fmt.Errorf("cannot read: %v", err)
+		}
+
+		if isEmpty {
+			return errors.New("content is empty")
+		}
+
+	case 2:
+		lg.Info("testing without flush")
+		// testing autoflush (dont flush with lg.Flush)
+
+		// sleeped 3 seconds (auto-flush delay is two seconds but im sleeping for three seconds)
+		time.Sleep(3 * time.Second)
+
+		err, isEmpty := isEmpty(path)
+		if err != nil {
+			return fmt.Errorf("cannot read: %v", err)
+		}
+
+		if isEmpty {
+			return errors.New("content is empty")
+		}
+
+	default:
+		return fmt.Errorf("%v is invalid step, only accept's 1||2", step)
+	}
+
+	return nil
+}
+
+func isEmpty(path string) (error, bool) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return err, true
+	}
+
+	var isEmpty bool
+
+	if string(content) == "" {
+		isEmpty = true
+	}
+
+	return nil, isEmpty
 }
